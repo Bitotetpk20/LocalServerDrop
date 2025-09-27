5980ca7b932dafb1f61ddc08f62ea265760ff1bf
Kristupas <112942415+KristupasJon@users.noreply.github.com>
2025-09-27 15:32:49 +0300

    Remove tray

--- main/index.js
+++ main/index.js
@@ -1,5 +1,5 @@
 const electron = require('electron');
-const { app, BrowserWindow, ipcMain, shell, screen, Tray, Menu } = electron;
+const { app, BrowserWindow, ipcMain, shell, screen } = electron;
 const path = require('path');
 const crypto = require('crypto');
 
@@ -10,16 +10,13 @@ process.env.LSD_ADMIN_TOKEN = ADMIN_TOKEN;
 // start express server and get server reference
 const { server } = require('../backend/server.js');
 
-let tray = null;
-let mainWindow = null;
-
 const createWindow = () => {
   const isDev = !app.isPackaged;
   
   // hopefully this works on all platforms
   const { width, height } = screen.getPrimaryDisplay().workAreaSize;
   
-  mainWindow = new BrowserWindow({
+  const win = new BrowserWindow({
     width: width,
     height: height,
     icon: isDev ? 
@@ -38,64 +35,9 @@ const createWindow = () => {
     path.join(__dirname, '../renderer/index.html') : 
     path.join(app.getAppPath(), 'renderer/index.html');
   
-  mainWindow.loadFile(htmlPath);
-  mainWindow.setMenuBarVisibility(false);
-
-  // minimize to tray probably wont work on macOS
-  mainWindow.on('close', (event) => {
-    if (!app.isQuiting) {
-      event.preventDefault();
-      mainWindow.hide();
-    }
-  });
-
-  createTray();
+  win.loadFile(htmlPath);
+  win.setMenuBarVisibility(false);
 }
-
-const createTray = () => {
-  const isDev = !app.isPackaged;
-  const iconPath = isDev ? 
-    path.join(__dirname, '../renderer/assets/icon.ico') :
-    path.join(app.getAppPath(), 'renderer/assets/icon.ico');
-    
-  tray = new Tray(iconPath);
-  
-  const contextMenu = Menu.buildFromTemplate([
-    {
-      label: 'Show LocalServerDrop',
-      click: () => {
-        mainWindow.show();
-        mainWindow.focus();
-      }
-    },
-    {
-      label: 'Hide',
-      click: () => {
-        mainWindow.hide();
-      }
-    },
-    { type: 'separator' },
-    {
-      label: 'Quit',
-      click: () => {
-        app.isQuiting = true;
-        app.quit();
-      }
-    }
-  ]);
-  
-  tray.setToolTip('LocalServerDrop - File sharing server');
-  tray.setContextMenu(contextMenu);
-  
-  tray.on('double-click', () => {
-    if (mainWindow.isVisible()) {
-      mainWindow.hide();
-    } else {
-      mainWindow.show();
-      mainWindow.focus();
-    }
-  });
-};
 
 // IPC Handlers
 
@@ -156,12 +98,8 @@ const shutdownServer = () => {
 };
 
 app.on('window-all-closed', () => {
-  if (app.isQuiting) {
-    shutdownServer();
-    if (process.platform !== 'darwin') {
-      app.quit();
-    }
-  }
+  shutdownServer();
+  app.quit(); // exit completely
 });
 
 app.on('before-quit', () => {
@@ -187,8 +125,5 @@ process.on('SIGTERM', () => {
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
-  } else if (mainWindow) {
-    mainWindow.show();
-    mainWindow.focus();
   }
 });
